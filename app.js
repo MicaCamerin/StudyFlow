235b4ddc3a186b0938ffa8b4578f50992a6154ad
Micaela Camerini <micaelacamerini19@gmail.com>
2025-06-13 10:26:36 -0300

    Redefinicion de funcion agregar tareas

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ saludo.innerText = "Hola, " + nombre;
 //Formulario - boton +//
 const btnAgregar = document.getElementById("botonAdd");
 
-//Evento click y prevenir recargar de la pagina//
-btnAgregar.addEventListener("click", (event) => {
- event.preventDefault();
-});
-
 //Array de tareas //
 let tareas = [];
 
@@ -19,55 +14,35 @@ const botonAdd = document.getElementById("botonAdd");
 const listaTareas = document.getElementById("listaTareas");
 const tareasVacio = document.getElementById("tareasVacio");
 
-//Funcion para mostrar las tareas//
+//Funcion para mostrar las tareas y eliminarlas//
 function mostrarTareas() {
   listaTareas.innerHTML="";
   if (tareas.length === 0) {
     tareasVacio.style.display = "block";
   } else {
     tareasVacio.style.display = "none";
-    tareas.forEach((tareas, index)=> {
+    tareas.forEach((tarea, index)=> {
       listaTareas.innerHTML += `<li>${index + 1}. ${tarea}</li>`;
     });
   }
 }
 
+//Evento click y prevenir recargar de la pagina//
+btnAgregar.addEventListener("click", (event) => { 
+ event.preventDefault();
+
 //Funcon para agregar tareas//
-function agregarTarea() {
-    let nuevaTarea = prompt("Agregar tarea");
+
+    let nuevaTarea = imputTare.value;
   
-    if (nuevaTarea !== null && nuevaTarea != "") { //Tuve que investigar sobre el null, ya que la consola me tiraba error al ingresar tareas//
+    if (nuevaTarea !== "") {
       tareas.push(nuevaTarea);
-      console.log("Tarea agregada");
+      imputTare.value ="";
       mostrarTareas();
     } else {
       alert("No se ingresaron tareas.");
     }
-  }
-
-//Funcion para eliminar tareas//
-function eliminarTarea() {
-  mostrarTareas();
-
-  let indice = prompt("¿Qué número de tarea querés eliminar?");
-  let posicion = parseInt(indice);
-  
-    if (posicion > 0 && posicion <= tareas.length) {
-      let nuevaLista = [];
-
-      for (let i = 0; i < tareas.length; i++) {
-        if (i != (posicion - 1)) {
-          nuevaLista.push(tareas[i]);
-        }
-      }
-      tareas = nuevaLista;
-  
-      console.log("Tarea eliminada.");
-      mostrarTareas();
-    } else {
-      alert("Número inválido. Por favor, elegí una tarea que exista.");
-    }
-  }
+});
 
 
 
